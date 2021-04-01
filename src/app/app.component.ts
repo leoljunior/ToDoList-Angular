b7bb806415ddb8065f96648bd7521e85caa8d78a
Leonardo Junior <juhhinx@gmail.com>
2021-04-01 20:31:46 -0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  public todos: Todo[] = []; // -> vazio  //public todos: any[]; -> undefined
+  public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(50),
+        Validators.required,
+      ])]
+    });
+    this.todos.push(new Todo(1, 'Passear cachorro', false));
+    this.todos.push(new Todo(2, 'Cortar cabelo', true));
+    this.todos.push(new Todo(3, 'Ir ao mercado', false));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
+
 }
